bdc8371b1a12b218aa4bdfada7df4e1f87c38d6e
Shodan <raphael.barthe@laposte.net>
2026-01-01 21:18:48 +0100

    Update install_apt.sh
    marstek_local_api installation added

--- resources/install_apt.sh
+++ resources/install_apt.sh
@@ -9,6 +9,20 @@ wget https://raw.githubusercontent.com/NebzHB/dependance.lib/master/pyenv.lib --
 . ${BASE_DIR}/pyenv.lib
 ##################################################################
 
+############ Recuperation lib marstek_local_api ###############
+TRDPARTY_DIR=$(realpath ${BASE_DIR}/../3rdparty)
+API_DIR=$(realpath ${TRDPARTY_DIR}/marstek_local_api)
+cd ${TRDPARTY_DIR}
+if [ -d ${API_DIR} ]
+then
+    rm -R marstek_local_api
+fi
+wget https://github.com/jaapp/ha-marstek-local-api/archive/refs/heads/master.zip
+unzip master.zip
+mv ha-marstek-local-api-master/custom_components/marstek_local_api marstek_local_api
+rm -R ha-marstek-local-api-master
+rm unzip master.zip
+
 TARGET_PYTHON_VERSION="3.10"
 # VENV_DIR=${BASE_DIR}/venv
 # APT_PACKAGES="first1 second2"
