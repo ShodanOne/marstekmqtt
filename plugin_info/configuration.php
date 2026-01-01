75f9d921ae593b280c37417be86783ae6545ac4e
Shodan <raphael.barthe@laposte.net>
2026-01-01 19:29:21 +0100

    Update configuration.php
    update

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -28,6 +28,35 @@ if (!isConnect()) {
     <legend><i class="fas fa-play-circle"></i> {{Parametrage}}</legend>
   
     <div class="form-group">
+  
+  	  <!-- Plugin Version -->
+	  <label class="col-md-2 control-label">{{Plugin version}}</label>
+	  <div class="col-md-1">
+        <?php
+         echo '<span class="label label-success"> '. marstekmqtt::getPluginVersion() . '</span>';
+        ?>
+	  </div>
+             
+      <!-- API Version -->
+	  <label class="col-md-2 control-label">{{API version}}</label>
+	  <div class="col-md-1">
+        <?php
+          echo '<span class="label label-success"> '. marstekmqtt::getApiVersion() . '</span>';
+        ?>
+	  </div>
+      
+      <!-- Configuration Demon distant -->
+      <label class="col-md-2 control-label">{{Demon distant}}
+        <sup><i class="fas fa-question-circle tooltips" title="{{Pour debug uniquement}}"></i></sup>
+      </label>
+      <div class="col-md-1">
+	    <input type="checkbox" class="configKey form-control" style="width:auto;" data-l1key="RemoteDemon" unchecked/>
+	  </div>
+          
+    </div>
+          
+    <div class="form-group">
+          
       <!-- Configuration du port API Marstek -->
       <label class="col-md-2 control-label">{{Port API Marstek}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le port UDP de l'API Marstek}}"></i></sup>
@@ -35,6 +64,7 @@ if (!isConnect()) {
       <div class="col-md-1">
         <input class="configKey form-control" data-l1key="apiPort" placeholder="30000"/>
       </div>
+          
       <!-- confiuguration timeout -->
       <label class="col-md-2 control-label">{{Timeout requete API (s)}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le temps d'attente maximum pour une réponse à une requete API}}"></i></sup>
@@ -42,6 +72,7 @@ if (!isConnect()) {
       <div class="col-md-1">
         <input class="configKey form-control" data-l1key="apiTimeout" placeholder="5"/>
       </div>
+          
       <!-- confiuguration retry -->
       <label class="col-md-2 control-label">{{Nb tentative requete API}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez le nombre de tentatives successives pour une requete API}}"></i></sup>
@@ -49,6 +80,7 @@ if (!isConnect()) {
       <div class="col-md-1">
         <input class="configKey form-control" data-l1key="apiRetry" placeholder="3"/>
       </div>
+          
       <!-- confiuguration periode -->
       <label class="col-md-2 control-label">{{Periode sondage (s)}}
         <sup><i class="fas fa-question-circle tooltips" title="{{Renseignez la periode de sondage des batteries}}"></i></sup>
@@ -56,20 +88,9 @@ if (!isConnect()) {
       <div class="col-md-1">
         <input class="configKey form-control" data-l1key="period" placeholder="10"/>
       </div>
-      <!-- Configuration Demon distant -->
-        <label class="col-md-2 control-label">{{Demon distant}}
-          <sup><i class="fas fa-question-circle tooltips" title="{{Pour debug uniquement}}"></i></sup>
-        </label>
-        <div class="col-md-1">
-			<input type="checkbox" class="configKey form-control" style="width:auto;" data-l1key="RemoteDemon" checked/>
-		</div>
-        <!-- Version -->
-		<label class="col-md-2 control-label">{{Version plugin}}</label>
-		<div class="col-md-1">
-            <?php
-            	echo '<span class="label label-success"> '. config::byKey('pluginVersion', 'marstekmqtt', 'N/A') . '</span>';
-            ?>
-		</div>
+          
     </div>
+          
   </fieldset>
+          
 </form>
